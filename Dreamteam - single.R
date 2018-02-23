5e20c993584f0f631256acaf2e3d94670d32f841
jtattersall09403 <jacktattersall92@gmail.com>
2018-02-23 10:53:43 +0000

    Added dreamteam

--- Dreamteam - single.R
+++ Dreamteam - single.R
@@ -4,6 +4,10 @@
 # Research into optimisation options not as positive as hoped. Brute force approach??
 
 ### How about recursively applying the single transfer algorithm?
+# Disadvantages: wastes money on bench players
+# Advantages: provides strength in depth, allows for different formations, first team xp is similar to the
+# first 11 optimisation approach (1 point lower, in one trial)
+
 optim_single <- function(myteam2, fpl.3, bank) {
   
   # Single transfers
@@ -73,5 +77,126 @@ opt_single %>% mutate(xp = ifelse(captain == 1, xp *2, xp)) %>% ungroup %>% summ
 
 trans1[!trans1$element %in% opt_single$element,]
 
+# --------------------- Optimise starting 11 ---------------
+
+# Strategy - make bench players as cheap as possible to allow more to spend on first team.
+# Achieve this by first picking your best possible current team.
+# Then transfer all bench players for their cheapest alternatives, updating the bank.
+# Then run the original process just on the starting 11.
+# Major limitation - restricts you to the formation the original team is playing in.
+
+### How about recursively applying the single transfer algorithm?
+optim_single <- function(trans0, fpl.3, bank) {
+  
+  # Single transfers
+  single <- trans0 %>%
+    inner_join(select(fpl.3, id, web_name, pos, now_cost, team, goalprob, xp), by = c('pos' = 'pos')) %>%
+    filter(!id %in% trans0$element,
+           now_cost <= price + bank) %>%
+    mutate(xpdiff = xp.y - xp.x) %>%
+    arrange(desc(xpdiff))
+  
+  # Check if transfer increases xp
+  if (single$xpdiff[1] > 0) {
+    # Choose transfers out and in
+    trans_out <- c(single$element[1])
+    trans_in <- c(single$id[1])
+    
+    # Ids after transfers
+    new_ids <- append(trans0$element[!trans0$element %in% trans_out], trans_in)
+    
+    # Get team after transfers
+    trans <- fpl.3[fpl.3$id %in% new_ids,] %>%
+      mutate(position = row_number(), dum =1, 'player_name'= web_name, captain = 0) %>%
+      select(position, player_name, now_cost, id, first_name.x, web_name, pos, team, goalprob, xp, dum, captain)
+    names(trans) <- names(trans0)
+    
+    # Update bank
+    bank <<- bank - (single$now_cost[1] - single$price[1])
+  } else {
+    
+    # Alert
+    print("Optimal team reached")
+    
+    # Return original team
+    trans <- trans0
+    
+  }
+  
+  return(trans)
+}
+
+
+
+# Set up first team for optimisation. Myteam3 is result from getBestTeam
+subs <- myteam2[!myteam2$element %in% myteam3$element,] %>%
+  inner_join(select(fpl.3, id, web_name, pos, now_cost, team, goalprob, xp), by = c('pos' = 'pos')) %>%
+  filter(!id %in% myteam2$element,
+         now_cost <= price + bank) %>%
+  mutate(xpdiff = xp.y - xp.x, pricediff = now_cost - price) %>%
+  arrange(pricediff, desc(xpdiff)) %>%
+  group_by(id) %>%
+  mutate(rank = row_number()) %>%
+  filter(rank == 1) %>% # De-dupe - each sub-in can only appear once
+  arrange(pricediff, desc(xpdiff)) %>%
+  group_by(element) %>% # Select one sub for each
+  mutate(rank = row_number()) %>%
+  filter(rank == 1)
+
+#Get ids of new subs
+newsubs <- myteam2[!myteam2$element %in% myteam3$element,] %>%
+  ungroup %>%
+  select(element) %>%
+  left_join(select(subs, element, id), by = 'element') %>%
+  mutate(id = ifelse(is.na(id), element, id)) %>%
+  select(id)
+
+# Get sub details
+subdeets <- fpl.3[fpl.3$id %in% newsubs$id,] %>%
+  arrange(ifelse(pos == 'Goalkeeper',0,1),desc(xp)) %>%
+  mutate(position = row_number(), order = row_number() + 11, 'player_name'= web_name, captain = 0) %>%
+  select(position, player_name, now_cost, id, first_name.x, web_name, pos, team, goalprob, xp, order, captain)
+
+
+# Need to append subs to final team
+
+# Initialise loop
+i <- 1
+bank <- 10
+bank <- bank - sum(subs$pricediff)
+iter_max <- 100
+trans0 <- myteam3
+trans1 <- optim_single(myteam3, fpl.3, bank)
+
+# Get best single transfer team
+while (sum(trans1$xp) > sum(trans0$xp) & i <= iter_max) {
+  trans0 <- trans1
+  trans1 <- optim_single(trans0, fpl.3, bank)
+  i <- i + 1
+  print(paste(i, "iterations processed"))
+}
+
+
+# Put in correct order, mark captain, and append subs
+names(subdeets) <- names(trans1)
+opt_single <- trans1 %>%
+  mutate(order = case_when(pos == 'Goalkeeper' ~ 1,
+                                       pos == 'Defender' ~ 2,
+                                       pos == 'Midfielder' ~ 3,
+                                       pos == 'Forward' ~ 4)) %>%
+  arrange(desc(xp)) %>%
+  mutate(captain = ifelse(row_number()==1, 1, 0)) %>%
+  arrange(order) %>%
+  rbind(subdeets)
+
+View(opt_single)
+
+# Total xp - yours vs dreamteam
+myteam3 %>% mutate(xp = ifelse(captain == 1, xp *2, xp)) %>% ungroup %>% summarise(xp = sum(xp)) %>% unlist
+opt_single %>% filter(order <= 11) %>% mutate(xp = ifelse(captain == 1, xp *2, xp)) %>% ungroup %>% summarise(xp = sum(xp)) %>% unlist
+
+# Check total price. Remember during the season this may be different to 100 mil.
+sum(opt_single$price)
+bank
 
 
--- Dreamteam.R
+++ Dreamteam.R
@@ -1,44 +1,117 @@
-# Messing around with dream team
-# Research into optimisation options not as positive as hoped. Brute force approach??
+# Try applying double transfer algorithm
+# Disadvantages - significantly slower. Wastes money on bench players.
+# Advantages - appears to converge in fewer iterations. More globally optimal than single transfer solution.
 
-### How about recursively applying the double transfer algorithm?
-# Get all squad pairs
-myteam2$dum <- 1
-squad <- myteam2 %>%
-  select(dum,  element, first_name.x, player_name, pos, team, price, xp) %>%
-  inner_join(myteam2, by = 'dum') %>%
-  filter(!(first_name.x.x == first_name.x.y & player_name.x == player_name.y)) %>%
+
+
+# Get all fpl pairs
+fpl.3$dum <- 1
+fplsquad <- fpl.3 %>%
+  select(dum, id,  first_name.x, second_name.x, pos, team, now_cost, xp) %>%
+  inner_join(select(fpl.3, id, dum, first_name.x, second_name.x, pos, team, now_cost, xp), by = 'dum') %>%
+  filter(!(first_name.x.x == first_name.x.y & second_name.x.x == second_name.x.y)) %>%
   mutate(pos = paste(pos.x, pos.y, sep="-"),
-         price = price.x + price.y,
+         price = now_cost.x + now_cost.y,
          xp = xp.x + xp.y) %>%
-  select(element.x, element.y,
+  select(id.x,
+         id.y,
          first_name.x.x,
-         player_name.x,
+         second_name.x.x,
          first_name.x.y,
-         player_name.y,
+         second_name.x.y,
          pos, price, xp)
 
-# Remove duplicates
-squad <- squad[!duplicated(data.frame(t(apply(squad[,c(2,4)], 1, sort)), squad$price)),]
+optim_double <- function(trans0, fpl.3, fplsquad, bank) {
+  
+  # Get all squad pairs
+  trans0$dum <- 1
+  squad <- trans0 %>%
+    select(dum,  element, first_name.x, player_name, pos, team, price, xp) %>%
+    inner_join(trans0, by = 'dum') %>%
+    filter(!(first_name.x.x == first_name.x.y & player_name.x == player_name.y)) %>%
+    mutate(pos = paste(pos.x, pos.y, sep="-"),
+           price = price.x + price.y,
+           xp = xp.x + xp.y) %>%
+    select(element.x,
+           first_name.x.x,
+           player_name.x,
+           element.y,
+           first_name.x.y,
+           player_name.y,
+           pos, price, xp)
+  
+  # Remove duplicates
+  squad <- squad[!duplicated(data.frame(t(apply(squad[,c(2,4)], 1, sort)), squad$price)),]
+  
+  # Double transfers
+  double <- inner_join(squad, fplsquad, by = 'pos') %>%
+    mutate(xpdiff = xp.y - xp.x) %>%
+    filter(price.x + bank >= price.y,
+           !id.x %in% trans0$element,
+           !id.y %in% trans0$element) %>%
+    group_by(pos) %>%
+    mutate(rank = rank(desc(xpdiff), ties.method = 'first')) %>%
+    filter(rank <= 5) %>%
+    arrange(desc(xpdiff))
+  
+  # Remove duplicates
+  if (nrow(double) > 0) double <- double[!duplicated(data.frame(t(apply(double[,c('second_name.x.x','second_name.x.y')], 1, sort)), double$price.x)),]
+  
+  # Check if transfer increases xp
+  if (double$xpdiff[1] > 0) {
+    # Choose transfers out and in
+    trans_out <- c(double$element.x[1], double$element.y[1])
+    trans_in <- c(double$id.x[1], double$id.y[1])
+    
+    # Ids after transfers
+    new_ids <- append(trans0$element[!trans0$element %in% trans_out], trans_in)
+    
+    # Get team after transfers
+    trans <- fpl.3[fpl.3$id %in% new_ids,] %>%
+      mutate(position = row_number(), dum =1, 'player_name'= web_name) %>%
+      select(position, player_name, now_cost, id, first_name.x, web_name, pos, team, goalprob, xp, dum)
+    names(trans) <- names(trans0)
+    
+    # Update bank
+    bank <<- bank - (double$price.y[1] - double$price.x[1])
+  } else {
+    
+    # Alert
+    print("Optimal team reached")
+    
+    # Return original team
+    trans <- trans0
+    
+  }
+  
+  return(trans)
+}
 
-# Join squad pairs to fpl pairs
-double_transfers <- inner_join(squad, fplsquad, by = 'pos') %>%
-  mutate(xpdiff = xp.y - xp.x) %>%
-  filter(price.x + bank >= price.y,
-         !id.x %in% myteam2$element,
-         !id.y %in% myteam2$element) %>%
-  group_by(pos) %>%
-  mutate(rank = rank(desc(xpdiff), ties.method = 'first')) %>%
-  filter(rank <= 5) %>%
-  arrange(desc(xpdiff))
 
-# Remove duplicates
-double_transfers <- double_transfers[!duplicated(data.frame(t(apply(double_transfers[,c('second_name.x.x','second_name.x.y')], 1, sort)), double_transfers$price.x)),]
 
-# Choose transfers out and in
-trans_out <- c(double_transfers$element.x[1], double_transfers$element.y[1])
-trans_in <- c(double_transfers$id.x[1], double_transfers$id.y[1])
+# Initialise loop
+i <- 0
+bank <- 10
+iter_max <- 20
+trans0 <- myteam2
+trans1 <- optim_double(trans0, fpl.3, fplsquad, bank)
 
-# Get team - make sure to update amount in bank
-myteam_1trans <- transfers(myteam2, fpl.3, trans_in, trans_out)
+# Get best single transfer team
+while (sum(trans1$xp) > sum(trans0$xp) & i <= iter_max) {
+  trans0 <- trans1
+  trans1 <- optim_double(trans0, fpl.3, fplsquad, bank)
+  i <- i + 1
+  print(paste(i, "iterations processed"))
+}
 
+View(trans1)
+
+# Get best starting 11
+opt_single <- getBestTeam(trans1)
+View(opt_single)
+
+# Total xp - yours vs dreamteam
+myteam3 %>% mutate(xp = ifelse(captain == 1, xp *2, xp)) %>% ungroup %>% summarise(xp = sum(xp)) %>% unlist
+opt_single %>% mutate(xp = ifelse(captain == 1, xp *2, xp)) %>% ungroup %>% summarise(xp = sum(xp)) %>% unlist
+
+trans1[!trans1$element %in% opt_single$element,]
